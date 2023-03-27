541ffabb2cffdadb3c72fbfcac15116f97576771
Alina <alina.zabaidulina@mail.ru>
2023-03-27 11:48:31 +0500

    2

--- ChromeDriver/lesson7_step8.py
+++ ChromeDriver/lesson7_step8.py
@@ -1,14 +1,13 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
 try:
-    #link1 = "http://suninjuly.github.io/registration1.html"
-    link2 = "http://suninjuly.github.io/registration2.html"
+    link1 = "http://suninjuly.github.io/registration1.html"
+    #link2 = "http://suninjuly.github.io/registration2.html"
     #тест проходит по ссылке1 и падает на ссылке2
 
     browser = webdriver.Chrome()
-    browser.get(link2)
+    browser.get(link1)
 
     first_name = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']").send_keys("Ivan")
     last_name = browser.find_element(By.XPATH, "//*[@placeholder='Input your last name']").send_keys("Ivanov")
@@ -17,7 +16,6 @@ try:
     address = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your address:']").send_keys("Russia")
 
     browser.find_element(By.CSS_SELECTOR, ".btn.btn-default").click()
-
 finally:
     time.sleep(5)
     browser.quit()
